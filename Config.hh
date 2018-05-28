af7557bc3dff680105bfadeaf1df495c87eb22de
Charmaine Ndolo <ndolocha@b-tu.de>
2018-05-28 17:44:33 +0200

    more code

--- Config.hh
+++ Config.hh
@@ -8,7 +8,7 @@ struct config_consts {
 	uint64_t VIRT; 
 } config_consts;
 
-class config {
+class config32phys {
 
 public:
 
@@ -31,7 +31,64 @@ public:
 	static uint64_t getPhys() {
 		return config_consts.PHYS;
 	}
+
 	static uint64_t getVirt() { 
 		return config_consts.VIRT;
 	}
 };
+class configOffset {
+	
+public:
+	static void setPhys(uint64_t phys) {
+		config_consts.PHYS = phys;
+	}
+
+	static void setVirt(uint64_t virt) {
+		config_consts.VIRT = virt;
+	}
+
+	static void setSize(size_t size) {
+		config_consts.SIZE = size;
+	}
+	
+	static size_t getSize() {
+		return config_consts.SIZE;
+	}
+
+	static uint64_t getPhys() {
+		return config_consts.PHYS;
+	}
+
+	static uint64_t getVirt() { 
+		return config_consts.VIRT;
+	}
+};
+
+class config64Phys {
+
+public:
+
+	static void setPhys(uint64_t phys) {
+		config_consts.PHYS = phys;
+	}
+
+	static void setVirt(uint64_t virt) {
+		config_consts.VIRT = virt;
+	}
+
+	static void setSize(size_t size) {
+		config_consts.SIZE = size;
+	}
+	
+	static size_t getSize() {
+		return config_consts.SIZE;
+	}
+
+	static uint64_t getPhys() {
+		return config_consts.PHYS;
+	}
+
+	static uint64_t getVirt() { 
+		return config_consts.VIRT;
+	}
+}; 
--- main.cc
+++ main.cc
@@ -14,24 +14,20 @@ int main(int argv, char* argc[]) {
 	const void* ptr = reinterpret_cast<const void*>(phys);
 	const void* ptr2 = reinterpret_cast<const void*>(phys+size+1);
 	const void* ptr3 = reinterpret_cast<const void*>(log + size);
-	
-	std::cout<<"phys: "<<config::getPhys()<<std::endl;
-	std::cout<<"size: "<<config::getSize()<<std::endl;
-	std::cout<<"virt: "<<config::getVirt()<<std::endl;
-	
+		
 	std::cout<<"phys: "<<ptr<<std::endl;
 	std::cout<<"size: "<<size<<std::endl;
 	std::cout<<"log: "<<log<<std::endl;
 	
-	config::setPhys(phys);
-	config::setVirt(log);
-	config::setSize(size);	
+	configOffset::setPhys(phys);
+	configOffset::setVirt(log);
+	configOffset::setSize(size);	
 	
-	std::cout<<"phys: "<<config::getPhys()<<std::endl;
-	std::cout<<"size: "<<config::getSize()<<std::endl;
-	std::cout<<"virt: "<<config::getVirt()<<std::endl;
-	
-	AbstractOffsetPtr* ap1;
+	std::cout<<"phys: "<<configOffset::getPhys()<<std::endl;
+	std::cout<<"size: "<<configOffset::getSize()<<std::endl;
+	std::cout<<"virt: "<<configOffset::getVirt()<<std::endl;
+		
+	AbstractOffsetPtr<configOffset>* ap1;
 	
 	bool val1 = ap1->isValidTypeAddress(ptr);
 	bool val2 = ap1->isValidTypeAddress(ptr2);
