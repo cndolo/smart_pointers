b96b900800b05a21516efdf825ee7f2909edde9e
Charmaine Ndolo <ndolocha@b-tu.de>
2018-06-13 13:57:31 +0200

    beginning of new implementation

--- Config.hh
+++ Config.hh
@@ -2,97 +2,50 @@
 #include <cstddef>
 #include <cstdint>
 
-struct config_consts{
-	uint64_t PHYS;
-	size_t SIZE;
-	uint64_t VIRT; 
+struct ImageConfig{
+	
+	static uint64_t PHYS_BASE;
+	static size_t SIZE;
+	static uint64_t VIRT_BASE; 
+
+	static uint64_t physBase() { return PHYS_BASE; }
+	static size_t size() { return SIZE; }
+	static uint64_t virtBase() { return VIRT_BASE; }
+
+	static void setPhysBase(uint64_t phys) { PHYS_BASE =phys; }
+	static void setSize(size_t size) { SIZE =size; }
+	static void setVirtBase(uint64_t virt) { VIRT_BASE =virt; }
 };
 
-config_consts off_config;
-config_consts phys32_config;
-config_consts phys64_config;
-
-class config32phys {
-
-public:
-
-	static void setPhys(uint64_t phys) {
-		off_config.PHYS = phys;
-	}
-
-	static void setVirt(uint64_t virt) {
-		off_config.VIRT = virt;
-	}
-
-	static void setSize(size_t size) {
-		off_config.SIZE = size;
-	}
+struct KernelConfig{
 	
-	static size_t getSize() {
-		return off_config.SIZE;
-	}
+	static uint64_t PHYS_BASE;
+	static size_t SIZE;
+	static uint64_t VIRT_BASE; 
 
-	static uint64_t getPhys() {
-		return off_config.PHYS;
-	}
+	static uint64_t physBase() { return PHYS_BASE; }
+	static size_t size() { return SIZE; }
+	static uint64_t virtBase() { return VIRT_BASE; }
 
-	static uint64_t getVirt() { 
-		return off_config.VIRT;
-	}
-};
-class configOffset {
-	
-public:
-	static void setPhys(uint64_t phys) {
-		phys32_config.PHYS = phys;
-	}
-
-	static void setVirt(uint64_t virt) {
-		phys32_config.VIRT = virt;
-	}
-
-	static void setSize(size_t size) {
-		phys32_config.SIZE = size;
-	}
-	
-	static size_t getSize() {
-		return phys32_config.SIZE;
-	}
-
-	static uint64_t getPhys() {
-		return phys32_config.PHYS;
-	}
-
-	static uint64_t getVirt() { 
-		return phys32_config.VIRT;
-	}
+	static void setPhysBase(uint64_t phys) { PHYS_BASE =phys; }
+	static void setSize(size_t size) { SIZE =size; }
+	static void setVirtBase(uint64_t virt) { VIRT_BASE =virt ;}
 };
 
-class config64phys {
-
-public:
-
-	static void setPhys(uint64_t phys) {
-		phys64_config.PHYS = phys;
-	}
-
-	static void setVirt(uint64_t virt) {
-		phys64_config.VIRT = virt;
-	}
-
-	static void setSize(size_t size) {
-		phys64_config.SIZE = size;
-	}
+struct MMIOConfig{
 	
-	static size_t getSize() {
-		return phys64_config.SIZE;
-	}
+	static uint64_t PHYS_BASE;
+	static size_t SIZE;
+	static uint64_t VIRT_BASE; 
 
-	static uint64_t getPhys() {
-		return phys64_config.PHYS;
-	}
+	static uint64_t physBase() { return PHYS_BASE; }
+	static size_t size() { return SIZE; }
+	static uint64_t virtBase() { return VIRT_BASE; }
 
-	static uint64_t getVirt() { 
-		return phys64_config.VIRT;
-	}
-}; 
+	static void setPhysBase(uint64_t phys) { PHYS_BASE =phys; }
+	static void setSize(size_t size) { SIZE =size; }
+	static void setVirtBase(uint64_t virt) { VIRT_BASE =virt; }
+};
+
+
+
--- main.cc
+++ main.cc
@@ -3,13 +3,7 @@
 #include <cstdint>
 #include <stdint.h>
 #include "Config.hh"
-#include "AbstractOffsetPtr.hh"
-#include "AbstractPhysPtr64.hh"
-#include "AbstractPhysPtr32.hh"
-#include "ImagePhysPtr32.hh"
-#include "KernelPhysPtr32.hh"
-#include "ImageOffsetPtr.hh"
-#include "KernelOffsetPtr.hh"
+//#include "AbstractOffsetPtr.hh"
 
 int main(int argv, char* argc[]) {
 
@@ -24,58 +18,7 @@ int main(int argv, char* argc[]) {
 	std::cout<<"phys: "<<ptr<<std::endl;
 	std::cout<<"size: "<<size<<std::endl;
 	std::cout<<"log: "<<log<<std::endl;
-	
-	configOffset::setPhys(phys);
-	configOffset::setVirt(log);
-	configOffset::setSize(size);	
-	
-	std::cout<<"phys: "<<configOffset::getPhys()<<std::endl;
-	std::cout<<"size: "<<configOffset::getSize()<<std::endl;
-	std::cout<<"virt: "<<configOffset::getVirt()<<std::endl;
-		
-	AbstractOffsetPtr<configOffset>* ap1;
-	
-	bool val1 = ap1->isKernelAddress(ptr);
-	bool val2 = ap1->isKernelAddress(ptr2);
-	bool val3 = ap1->isImageAddress(ptr3);
-	
-	std::cout<<ptr<<" valid: "<<val1<<std::endl;
-	std::cout<<ptr2<<" valid: "<<val2<<std::endl;
-	std::cout<<ptr3<<" valid: "<<val3<<std::endl;
 
-	AbstractPhysPtr64<config64phys>* apPhys;
-
-	std::cout<<"size of config64phys: "<<sizeof(config64phys)<<std::endl;
-	std::cout<<"size of AbstractPhysPtr64: "<<sizeof(apPhys)<<std::endl;
-		
-	bool val4 = apPhys->isKernelAddress(ptr);
-	bool val5 = apPhys->isKernelAddress(ptr2);
-	bool val6 = apPhys->isKernelAddress(ptr3);
-
-	std::cout<<ptr<<" valid: "<<val4<<std::endl;
-	std::cout<<ptr2<<" valid: "<<val5<<std::endl;
-	std::cout<<ptr3<<" valid: "<<val6<<std::endl;
-
-	AbstractPhysPtr32<config32phys>* apPhys32;
-
-	std::cout<<"size of AbstractPhysPtr32: "<<sizeof(apPhys32)<<std::endl;
-	std::cout<<"phys: "<<config32phys::getPhys()<<std::endl;
-	std::cout<<"size: "<<config32phys::getSize()<<std::endl;
-	std::cout<<"virt: "<<config32phys::getVirt()<<std::endl;
-
-	bool val7 = apPhys32->isKernelAddress(ptr);
-	bool val8 = apPhys32->isImageAddress(ptr2);
-	bool val9 = apPhys32->isKernelAddress(ptr3);
-
-	std::cout<<ptr<<" valid: "<<val7<<std::endl;
-	std::cout<<ptr2<<" valid: "<<val8<<std::endl;
-	std::cout<<ptr3<<" valid: "<<val9<<std::endl;
-
-	ImagePhysPtr32* ip = new ImagePhysPtr32();
-	KernelPhysPtr32* kp = new KernelPhysPtr32();
-	ImageOffsetPtr* io = new ImageOffsetPtr();
-	KernelOffsetPtr* ko = new KernelOffsetPtr();
-	
-	bool val10 = io->isKernelAddress(ptr);
-	std::cout<<ptr<<" valid: "<<val10<<std::endl;
+	uint64_t physBase = ImageConfig::physBase();
+	std::cout<<"physBase: "<<physBase<<std::endl;	
 }
